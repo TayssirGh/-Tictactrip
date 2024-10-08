b05cc76950d319a2675ed44a7693157fa187d314
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2024-10-08 18:34:39 +0100

    [FIXED] token extraction from authorization header

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -3,7 +3,8 @@ import jwt from 'jsonwebtoken';
 const secret = 'your-secret-key';
 
 export const authenticateToken = (req : any, res : any, next : any) => {
-    const token = req.headers['authorization'];
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
     if (!token) return res.sendStatus(403);
 
     jwt.verify(token, secret, (err : any, user : any) => {
